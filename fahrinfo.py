7c9576e93e2ff31784b170450e5c657eb985dfe8
ablepharus <ablepharus@gmail.com>
2017-09-14 20:05:58 +0200

    use different bvg backend

--- fahrinfo.py
+++ fahrinfo.py
@@ -58,8 +58,7 @@ def search_request(von, nach, zeit=datetime.datetime.now()):
                    'REQ0Total_KissRide_maxDist': 1000000,
                    'start': 'Suchen'
            })
-    print(_init)
-    print(r.content)
+
     return r
 
 
@@ -72,84 +71,27 @@ def search(von, nach, zeit=datetime.datetime.now()):
     for i in etree.xpath('.//a[starts-with(@id, "linkDtlC")]/@href'):
         ri = s.get(url + i)
         ei = lxml.html.fromstring(ri.content)
-        print(ei.cssselect('tr.tpDetails'))
+        details  = ei.cssselect('tr.tpDetails')
+        _first = lambda x: x[0] if isinstance(x, list) and len(x) > 0 else None
+        entries = []
+        info = None
 
-    connections, infos = [], []
-    for href in etree.xpath('//td[starts-with(@class,con)]/a/@href'):
-        if href.startswith('/Fahrinfo/bin/query'):
-            connections.append(href)
-        if href.startswith('/Fahrinfo/bin/query'):
-            infos.append(href)
+        print('-' * 80)
+        for d in details:
 
-    if len(connections) == 0:
-        print(f'no connections found: {r} {r.content.decode()}')
-    for connection in connections:
-        # print_connection_infos(connection)
-        connection_infos(connection)
+            if d.xpath('@class') == ['tpDetails sectionInfo']:
+                print(d.xpath('td[@class="remarks"]/span/strong/text()')[0])
 
-
-def connection_infos(url):
-    ''' get information of a specific connection, which may include
-        several steps
-    '''
-    d = dict()
-    r = s.get(base_url + url)
-    etree = lxml.html.fromstring(r.content)
-
-    print('-' * 80)
-    for part in etree.xpath('//p[starts-with(@class,"con")]'):
-        text = part.xpath('normalize-space()')
-
-        a = [(a.xpath('./@href')[0],
-              ''.join(list(a.xpath('./text()'))), a)
-             for a in part.xpath('.//a')]
-        for href, text, a in a:
-            # get whole text and clean it
-            text = text.replace('\n', '')
-            while text.find('  ') != -1:
-                text = text.replace('  ', ' ')
-
-            if href.startswith('/Fahrinfo/bin/traininfo'):
-                d['direction'], d['train'] = text, a.xpath('strong/text()')[0]
-            elif href.find('content/standortplaene') != -1:
-                d['lageplan'] = href
-            elif href.startswith('/Fahrinfo/bin/help.bin/dox?tpl=streckenin'):
-                d['info'] = href
-            elif href.find('MapLocation') != -1:
-                d['falkmap'] = href
-            elif href.startswith('//fahrinfo.bvg.de/Fahrinfo/bin/stboard.bin'):
-                d['more'] = href
-            elif text == 'Fußweg':
-                print('walk: {href}')
             else:
-                print(f'"{text}" - {href}')
-        print(etree.cssselect('.station'))
-        an_ab = ''.join(part.xpath('text()')).replace('\n', '')
-        an, ab = re.search('an (\d+):(\d+)', an_ab), \
-                 re.search('ab (\d+):(\d+)', an_ab)
+                timeValue = d.xpath('normalize-space(td[@class="timeValue"]/text())')
+                realTime = _first(d.xpath('td[@class="realTime"]/.//text()'))
+                platform = _first(d.xpath('td[@class="platform"]/text()'))
+                station = _first(d.xpath('td[@class="station"]/a/text()'))
+                platform = platform if platform is not None else ''
 
-        try:
-            anh, anm = an[1], an[2]
-            abh, abm = ab[1], ab[2]
-            print(f'{d["train"]}: {d["direction"]} ab: {abh}:{abm} an: {anh}:{anm}')
-        except TypeError:
-            m = re.search('(\d+) Min\.', an_ab)
-            try:
-                print(f'::: in {m[1]} minutes')
-            except:
-                print(f'could not parse {an_ab}')
+                print(f'{station} at {timeValue}({realTime}) {platform}')
 
-
-def print_connection_infos(url):
-    '''
-    '''
-    r = s.get(base_url + url)
-    etree = lxml.html.fromstring(r.content)
-
-    print('-' * 80)
-    for part in etree.xpath('//p[starts-with(@class,"con")]'):
-        text = part.xpath('normalize-space()')
-        print(text)
+    return
 
 
 def search_station(name):
